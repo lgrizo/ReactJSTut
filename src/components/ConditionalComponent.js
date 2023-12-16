982e14eae387acb633bd0bd758ca61e5d50fa6d4
Luis Rizo <rizo.luis@yahoo.com>
2023-12-15 21:58:36 -0500

    finished part 21

--- src/components/ConditionalComponent.js
+++ src/components/ConditionalComponent.js
@@ -1,3 +1,20 @@
+import { useState } from "react";
+import Counter from "./Counter";
+import FunctionalCounter from "./FunctionalCounter";
+
 export default function ConditionalComponent() {
-  return <div>This is a conditional component.</div>;
+  const [display, setDisplay] = useState(true);
+  if (display) {
+    return (
+      <div>
+        <h3>This is a conditional component.</h3>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h3>Nothing to see here.</h3>
+      </div>
+    );
+  }
 }
